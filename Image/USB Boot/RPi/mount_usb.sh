34d658233cbefcc41dc5bcf9a22806d84d785202
karannayan <karan25991@gmail.com>
2014-03-17 19:39:52 +0530

    errors saved to file from the USB mount script

--- Image/USB Boot/RPi/mount_usb.sh
+++ Image/USB Boot/RPi/mount_usb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 d=$(fdisk -l /dev/sd? |grep -E -o "/dev/sd[a-z][0-9]+")
 mount -t vfat $d /mnt/usb_hd/
-python /mnt/usb_hd/program.py >> /mnt/usb_hd/output.txt
+python /mnt/usb_hd/program.py > /mnt/usb_hd/output.txt 2> /mnt/usb_hd/error.txt
 umount $d
