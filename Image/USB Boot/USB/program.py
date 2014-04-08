db927625137aa33e204e4351818f82a2c7369697
Dexter Industries <dxtrindustries@gmail.com>
2014-04-07 21:20:07 -0400

    Updated program.py for USB Bootloader

--- Image/USB Boot/USB/program.py
+++ Image/USB Boot/USB/program.py
@@ -1,5 +1,5 @@
 # Example program
-# Prints Hello 10 times in the debug file, "output.txt"
+# Prints Hello 2 times in the debug file, "output.txt"
 
 
 import time
@@ -12,8 +12,8 @@ BrickPi.MotorEnable[PORT_B] = 1 #Enable the Motor B
 
 BrickPiSetupSensors()   #Send the properties of sensors to BrickPi
 
-for i in range(10):
-	print "hello \n"
+for i in range(2):
+    print "hello \n"
     print "Running Forward \n"
     BrickPi.MotorSpeed[PORT_A] = 200  #Set the speed of MotorA (-255 to 255)
     BrickPi.MotorSpeed[PORT_B] = 200  #Set the speed of MotorB (-255 to 255)
