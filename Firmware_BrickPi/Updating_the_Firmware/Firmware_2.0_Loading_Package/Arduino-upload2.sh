43022c74202e725d7f54ea14ddbd079e5b12bb49
Dexter Industries <dxtrindustries@gmail.com>
2014-07-24 14:01:13 +0530

    Added Arduino Upload Scripts
    Scripts for firmware uploading from Raspberry Pi to BrickPi.

---
+++ Firmware_BrickPi/Updating_the_Firmware/Firmware_2.0_Loading_Package/Arduino-upload2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+avrdude -P /dev/ttyACM0 -b 19200 -c avrisp -p m328p -e
+read -p "Press [Enter] key to continue ..."
+avrdude -P /dev/ttyACM0 -b 19200 -c avrisp -p m328p -U lfuse:w:0xFF:m
+read -p "Press [Enter] key to continue ..."
+avrdude -P /dev/ttyACM0 -b 19200 -c avrisp -p m328p -U hfuse:w:0xDA:m
+read -p "Press [Enter] key to continue ..."
+avrdude -P /dev/ttyACM0 -b 19200 -c avrisp -p m328p -U efuse:w:0x05:m
+read -p "Press [Enter] key to continue ..."
+avrdude -P /dev/ttyACM0 -b 19200 -c avrisp -p m328p -U flash:w:ATmegaBOOT_168_atmega328.hex
+read -p "Press [Enter] key to continue ..."
+avrdude -P /dev/ttyACM0 -b 19200 -c avrisp -p m328p -U flash:w:BrickPiFW_Compressed_Communication.cpp.hex
+read -p "Press [Enter] key to continue ..."
+avrdude -P /dev/ttyACM0 -b 19200 -c avrisp -p m328p -U eeprom:w:BrickPi_EEPROM_UC2.hex
+read -p "Press [Enter] key to continue ..."
